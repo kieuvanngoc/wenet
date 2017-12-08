dd25890aa40cb7ddfd00fe06748f0be163e1b48d
Kieu Van Ngoc <ngockv@gmail.com>
2017-12-08 09:11:52 +0700

    s

--- include/config.php
+++ include/config.php
@@ -7,10 +7,9 @@
  */
 $debug = 0;
 
-define('PG_URL_HOMEPAGE', 'http://localhost/crm/');
-//define('PG_URL_HOMEPAGE', 'http://zama.vn/');
-define('ROOT_URL', "http://localhost/crm/");
-define('DIR_ROOT', 'D:/wamp/www/crm/');
+define('PG_URL_HOMEPAGE', 'http://localhost/wenet/');
+define('ROOT_URL', "http://localhost/wenet/");
+define('DIR_ROOT', 'D:/wamp/www/wenet/');
 define('DOMAIN', 'trangchuchuatinh');
 
 /*-- MYSQL --*/
@@ -21,25 +20,12 @@ $database_username = 'root';
 $database_password = '';
 $database_name = 'wenet_crm';
 
-// WENET
-//$database_host = '123.31.45.231:3306';
-//$database_username = 'wenet_crm';
-//$database_password = 'wenet_crm123@123!@#';
-//$database_name = 'wenet_crm';
-
-// SERVER
-//$database_host = '42.112.27.32:33223';
-////$database_host = '192.168.1.223:3306';
-//$database_username = 'db_idea';
-//$database_password = 'Dtv3ds5S';
-//$database_name = 'db_idea';
-
 define('MEMCACHE_ON', 0);
 define('REDIS_ON', 0);
 define('PAGE_CACHE', 0); // Cache cho smarty
 define('CACHE_ON', 0);
 define('CACHE_DB', 0);
-define('CACHE_SERVER_NAME', 'localhost/crm/');
+define('CACHE_SERVER_NAME', 'localhost/wenet/');
 
 define('SETTING_DEBUG_PAPGE', 1);
 define('REWRITE_ON', 1);
